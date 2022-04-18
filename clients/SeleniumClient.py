b089c83f96284cb17b888d3575294c1cb2bb3ffc
jmjeon94 <jmjeon3155@gmail.com>
2022-04-18 19:34:21 +0900

    [FIX] windows 크롬 버그 수정

--- clients/SeleniumClient.py
+++ clients/SeleniumClient.py
@@ -15,6 +15,7 @@ class SeleniumClient:
         options = webdriver.ChromeOptions()
         if is_hide:
             options.add_argument("headless") # 창 숨기기
+            options.add_argument("no-sandbox")
             options.add_argument("user-agent=Mozilla/5.0 (Macintosh; Intel Mac OS X 10_12_6) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/61.0.3163.100 Safari/537.36") # for google 로그인 화면
 
         options.add_argument("window-size=1920,1080")  # for notion 로그인 버튼
